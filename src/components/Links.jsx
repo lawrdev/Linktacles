ed09cb45d3b238abf02116b6a2d1897f57292f97
Unorthodox <lawrdev@gmail.com>
2022-10-30 15:12:25 +0100

    created links section

---
+++ src/components/Links.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+
+const linkList = [
+  {
+    title: "Zuri Team",
+    id: "btn__zuri",
+    url: "https://training.zuri.team/",
+    disbaled: false,
+  },
+  {
+    title: "Zuri Books",
+    id: "books",
+    url: "http://books.zuri.team/",
+    disbaled: false,
+  },
+  {
+    title: "Python Books",
+    id: "book__python",
+    url: "https://books.zuri.team/python-for-beginners?red_id=lawrdev",
+    disbaled: false,
+  },
+  {
+    title: "Background Check for Coders",
+    id: "pitch",
+    url: "https://background.zuri.team",
+    disbaled: false,
+  },
+  {
+    title: "Design Books",
+    id: "book__design",
+    url: "https://books.zuri.team/design-rules",
+    disbaled: false,
+  },
+];
+function Links() {
+  return (
+    <>
+      <div>
+        {linkList.map((item, index) => (
+          <a
+            key={index}
+            href={item.url}
+            id={item.id}
+            target="_blank"
+            rel="noreferrer"
+            className={item.disbaled ? "btn_link disabled_link" : "btn_link"}
+          >
+            {item.title}
+          </a>
+        ))}
+      </div>
+    </>
+  );
+}
+
+export default Links;
