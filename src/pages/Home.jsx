5c982d98af632f488516ca7f80f082a8acf851f6
Unorthodox <lawrdev@gmail.com>
2022-10-30 03:30:23 +0100

    added Home.jsx page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import Profile from "../components/Profile";
+
+function Home() {
+  return (
+    <>
+      {/* Profile section */}
+      <div className="mt-12 max-w-6xl mx-auto">
+        <Profile />
+      </div>
+    </>
+  );
+}
+
+export default Home;
