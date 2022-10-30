f9ec3661b5ea65fcbfd2bd8f29e106e2027df72c
Unorthodox <lawrdev@gmail.com>
2022-10-30 15:16:02 +0100

    updated styles for social section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,22 @@
+import Links from "../components/Links";
 import Profile from "../components/Profile";
+import SlackImg from "../assets/img/slack.png";
+import GithubImg from "../assets/img/github.png";
 
 function Home() {
   return (
     <>
-      {/* Profile section */}
       <div className="mt-12 max-w-6xl mx-auto">
         <Profile />
+        <Links />
+
+        {/* Social section */}
+        <section className="py-6">
+          <div className="flex justify-center items-center gap-6">
+            <img src={SlackImg} alt="slack" width="24px" height="24px" />
+            <img src={GithubImg} alt="slack" width="24px" height="24px" />
+          </div>
+        </section>
       </div>
     </>
   );
