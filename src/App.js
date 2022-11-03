b4074299bf1d87f1fb5bb3dc0ac06db99414a447
Unorthodox <lawrdev@gmail.com>
2022-11-03 10:57:56 +0100

    build: adds react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home';
+import { Contact } from './pages/Contact'
+import Footer from "./components/Footer";
 
 function App() {
-  return (
+  return (<>
     <div className='px-4'>
-      <Home />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/contact' element={<Contact />} />
+      </Routes>
     </div>
-  );
+
+    <Footer />
+  </>);
 }
 
 export default App;
