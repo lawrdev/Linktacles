7a7b16b1db170e9ae584134dae82ef144fe794a5
Unorthodox <lawrdev@gmail.com>
2022-11-03 11:04:05 +0100

    feat: creates contact page

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,17 @@
+export function Contact() {
+  return (
+    <>
+      <section>
+        <div>
+          <h2 className="text-4xl font-semibold text-gray-900">Contact Me</h2>
+          <p className="text-xl text-gray-600">
+            Hi there, contact me to ask me about anything you have in mind.
+          </p>
+        </div>
+
+        {/* form */}
+        <div></div>
+      </section>
+    </>
+  );
+}
